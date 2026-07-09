00e9f1767c7d63745db45eaa2794263459479634
arshia <your-email@example.com>
2026-07-10 00:02:10 +0330

    This is my first commit and I want to start the project and now I have started the student panel section.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./App.css"
+import DashboardAthlete from './pages/Dashboard-Athlete/DashboardAthlete'
+export default function App() {
+  return (
+    <div>
+      <DashboardAthlete/>
+    </div>
+  )
+}
---
+++ src/pages/Dashboard-Athlete/DashboardAthlete.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashboardAthlete() {
+  return (
+    <div>
+      Dash
+    </div>
+  )
+}
